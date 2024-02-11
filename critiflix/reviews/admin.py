9c3feed1d06f5bfd15edfe2cf6fbea705f4ce16b
Pawel Paprzycki <pawel.paprzycki94@gmail.com>
2024-02-11 14:16:11 +0100

    Add custom admin site

--- critiflix/reviews/admin.py
+++ critiflix/reviews/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-# Register your models here.
+from reviews.models import Movie, MovieCasts, Review, Actor
+
+
+class CritiflixAdminSite(AdminSite):
+    title_header = 'Aplikacja administracyjna Critiflix'
+    site_header = 'Aplikacja administracyjna Critiflix'
+    index_title = 'Administracja witryną Critiflix'
+
+
+movie_admin_site = CritiflixAdminSite(name='critiflix')
+
+movie_admin_site.register(Movie)
+movie_admin_site.register(MovieCasts)
+movie_admin_site.register(Review)
+movie_admin_site.register(Actor)
--- critiflix/reviews/urls.py
+++ critiflix/reviews/urls.py
@@ -1,11 +1,14 @@
-from django.contrib import admin
+from reviews.admin import movie_admin_site
 from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path('admin/', movie_admin_site.urls),
+
     path('movies/', views.movie_list, name = 'movie_list'),
 
    path('movies/<int:pk>', views.movie_detail, name = 'movie_detail'),
 
    path('', views.base, name = 'base')
    ]
+
